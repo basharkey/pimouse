731ef2f7405222707d699b64d57a1377468f4633
basharkey <15859442+basharkey@users.noreply.github.com>
2022-12-07 21:17:17 +0000

    fixed hid descriptor on windows

--- gadget/gadget.go
+++ gadget/gadget.go
@@ -31,9 +31,9 @@ func Initialize() error {
         {filepath.Join(dirConfigs, dirStrings, "configuration"), "Config 1: ECM network"},
         {filepath.Join(dirConfigs, "MaxPower"), "250"},
 
-        {filepath.Join(dirFunctions, "protocol"), "1"},
+        {filepath.Join(dirFunctions, "protocol"), "2"},
         {filepath.Join(dirFunctions, "subclass"), "1"},
-        {filepath.Join(dirFunctions, "report_length"), "8"},
+        {filepath.Join(dirFunctions, "report_length"), "4"},
         // sudo usbhid-dump -d beaf | tail -n +2 | xxd -r -p | hidrd-convert -o spec
         {filepath.Join(dirFunctions, "report_desc"),
             "\x05\x01" + // Usage Page (Desktop)
@@ -59,26 +59,13 @@ func Initialize() error {
             "\x75\x08" + // Report Size (8)
             "\x95\x02" + // Report Count (2)
             "\x81\x06" + // Input (Data, Var, Rel)
-            // mouse vertical scroll resolution multipler
-            "\xa1\x02" + //  Collection (Logical)
-            "\x09\x48" + //  Usage (Resolution Multiplier)
-            "\x15\x00" + //  Logical Minimum (0)
-            "\x25\x01" + //  Logical Maximum (1)
-            "\x35\x01" + //  Physical Minimum (1)
-            "\x45\x04" + //  Physical Maximum (4)
-            "\x75\x02" + //  Report Size (2)
-            "\x95\x01" + //  Report Count (1)
-            "\xa4"     + //  Push
-            "\xb1\x02" + //  Feature (Data, Var, Abs)
             // mouse vertical scroll
             "\x09\x38" + // Usage (Wheel)
             "\x15\x81" + // Logical Minimum (-127)
             "\x25\x7f" + // Logical Maximum (127)
-            "\x35\x00" + // Physical Minimum (0)
-            "\x45\x00" + // Physical Maximum (0)
             "\x75\x08" + // Report Size (8)
+            "\x95\x01" + // Report Size (1)
             "\x81\x06" + // Input (Data, Var, Rel)
-            "\xc0"     + // End Collection
             "\xc0"     + // End Collection
             "\xc0"},     // End Collection
     }
